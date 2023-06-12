bf76e372b250c95d1090bdc51d8285f350ae3b0e
Ronvic2303 <58205103+ronvic12@users.noreply.github.com>
2023-06-11 21:54:09 -0700

    Updated Experience Resume

--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -10,28 +10,19 @@ const Experience = () => {
   <div className = "experience-container">
   <h1>Work Experience</h1>
   <VerticalTimeline>
-  <VerticalTimelineElement 
-    className = "vertical-timeline-element--work"
-    date ="August 2022 - Present"
-    contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-    contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
-    iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-    icon = {<WorkIcon/>}
-    >
+  <VerticalTimelineElement className = "vertical-timeline-element--work" date ="August 2022 - Present" contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }} contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }} iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }} icon = {<WorkIcon/>}>
     <h3 className="vertical-timeline-element-title">Software Engineer</h3>
     <h4 className="vertical-timeline-element-subtitle">Four Wheel Campers</h4>
     <p>
 
     <li>
-    Maintaining and developing two web application system(ERP and Four Wheel Drive) to help and create an efficient system for every department of the company(i.e. Sales(U.S.A,Germany,Canada and Taiwan),Services,Production,Accounting,Engineering,CNC Operators,etc.): Four wheel Drive is specifically made for Sales Department and Services while ERP is made for Production, Accounting,Engineering,Services and CNC Operators.
+    Maintained and Developed full-stack web applications, coding at all levels from database management(MYSQL) to the backend(Node.js) to the front-end(HTML/CSS/Javascript) to create an efficient system for every department of the company: Sales,Services,Production,Accounting,Engineering,Adminstrator.
     </li> 
     <li>
-    Collaborating with other engineers (i.e. mechanical) to put their new implemented CNC Drawings or fixes into the web application system.
+    Collaborated with other engineers (i.e. mechanical) to put their new implemented CNC Drawings or fixes into the web application system and communicate with every department to create a new idea on how to improve their web application system.
     </li>   
     <li>
-    Developing ERP and Four Wheel Drive web application via JavaScript, HTML,CSS for the
-Front-End development and NodeJS for backend. Uses MYSQL as the main source of database for support to make a report. Microsoft VBA and Excel(xlsx-populate to populate data into excel from javascript) for populating the data to support the Production department. </li>  
-    
+    Created database schemas and wrote efficient retrieval queries in MYSQL for various large scalable business requirements.    
     </p>
     </VerticalTimelineElement>
 
@@ -47,51 +38,33 @@ Front-End development and NodeJS for backend. Uses MYSQL as the main source of d
     <h4 className="vertical-timeline-element-subtitle">EyeVocab(Language Center, UC Davis)</h4>
     <p>
     <li>
-    Collaborating with two interns and the lead programmer developing Eyevocab project which is a cloud-based, open-source, digital
-vocabulary program, designed for students to learn, practice, and test their knowledge of target language lexical items. The program features spaced repetition, custom learning schedules, simultaneous engagement of seeing, hearing, and typing L2 vocabulary in order to maximize long-term lexical retention.
- 
+    Collaborated with two interns and the lead programmer to develop Eyevocab project which is a cloud-based, open-source, digital vocabulary program, designed for students to learn, practice, and test their knowledge of target language lexical items. The program features spaced repetition, custom learning schedules, simultaneous engagement of seeing, hearing, and typing L2 vocabulary in order to maximize long-term lexical retention.
     </li>   
     <li>
-    Implementing the web application using TypeScript/JavaScript, HTML,CSS and ReactJS Framework for the
-Front-End development. We use RethinkDB as our main source of Database, and using Express.js, Node.js, Next.js, and Docker for the backend development.
-    </li>  
+    Implemented the web application using TypeScript/JavaScript, HTML,CSS and ReactJS Framework for the Front-End development. We use RethinkDB as our main source of Database, and using Express.js, Node.js, Next.js, and Docker for the backend development.    </li>  
     
     </p>
     </VerticalTimelineElement>
 
 
 
-    <VerticalTimelineElement 
-    className = "vertical-timeline-element--work"
-    date ="May 2021 -November 2021"
-    contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-    contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
-    iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-    icon = {<WorkIcon/>}
-    >
+    <VerticalTimelineElement className = "vertical-timeline-element--work" date ="May 2021 -November 2021" contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }} contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }} iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }} icon = {<WorkIcon/>}>
     <h3 className="vertical-timeline-element-title">Software Developer</h3>
     <h4 className="vertical-timeline-element-subtitle">OpportunityKnocks</h4>
     <p>
     <li>
-    Design and implement the UI/UX of the app using Flutter and Dart languages to connect student-athletes to sponsors.
+    Designed and implemented the UI/UX of the app using Flutter and Dart languages to connect student-athletes to sponsors.
     </li>   
     <li>
-    Implement the back-end of the app using Firebase in order to make user profiles and Discount postings.   
+    Implemented the back-end of the app using Firebase in order to make user profiles and Discount postings. 
     </li>  
     <li>
-    Work with a team of 4+ developers to make an app in order to provide compensation to student-athletes and benefit businesses.
+    Worked with a team of 4+ developers to make an app in order to provide compensation to student-athletes and benefit businesses.
     </li>  
     
     </p>
     </VerticalTimelineElement>
-    <VerticalTimelineElement 
-    className = "vertical-timeline-element--work"
-    date ="October 2020-May 2021"
-    contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-    contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
-    iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-    icon = {<WorkIcon/>}
-    >
+    <VerticalTimelineElement className = "vertical-timeline-element--work" date ="October 2020-May 2021" contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }} contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }} iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }} icon = {<WorkIcon/>}>
     <h3 className="vertical-timeline-element-title">
 Front-End Developer</h3>
     <h4 className="vertical-timeline-element-subtitle">#include @ Davis</h4>
