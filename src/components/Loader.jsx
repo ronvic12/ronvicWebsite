d3af0cba205a86512b419d25389641ab93aaf76c
Ronvic2303 <58205103+ronvic12@users.noreply.github.com>
2024-09-13 01:04:38 -0700

    working on 3d front page

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -5,7 +5,7 @@ const Loader = () => {
     <Html>
 
     <div className= "flex justify-center items-center">
-        <div className= "w-20 h-20 border-2 border-opacity-20 border-blue-500 border-t-blue-500 animate-spin"/>        
+        <div className= "w-20 h-20 border-2 border-opacity-20 border-blue-500 border-t-blue-500 rounded-full animate-spin"/>        
     </div>
 
     </Html>
